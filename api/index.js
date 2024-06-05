f8981d80cbc0800cfdb1b44e10da295d3083205a
NuminousCode <sanchezwebdev@gmail.com>
2024-06-05 07:08:23 -0500

    modify server port

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const port = process.env.PORT || 3001;
 app.get('/', (req, res) => (res.sendFile(path.join(__dirname, 'public/index.html') )));
 
 app.get('/data', (req, res) => {
@@ -16,6 +17,6 @@ app.get('/data', (req, res) => {
     res.json(data);
 });
 
-app.listen(3001, () => console.log("Server ready on port 3001."));
+app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
 module.exports = app;
