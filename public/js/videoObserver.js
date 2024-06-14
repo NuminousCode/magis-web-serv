b885a76731e4daa171bade469c68f0067ee72a73
NuminousCode <sanchezwebdev@gmail.com>
2024-06-14 09:06:31 -0500

    css in head attempt

--- public/js/videoObserver.js
+++ public/js/videoObserver.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const placeholder = document.getElementById('bgPlaceholder');
     video.addEventListener('canplaythrough', function() {
         video.style.opacity = '1';
-        placeholder.style.opacity = '0';
+        // placeholder.style.opacity = '0';
         // setTimeout(() => {
         //     placeholder.style.display = 'none';
         // }, 1000); // Matches the transition duration
